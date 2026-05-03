88b3d0427bd7af4ba39610aa3a72aa11efdbf34c
artemmyr2002-byte <artemmyr2002@gmail.com>
2026-05-03 16:26:19 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDVbJwMeX0FTZfC7NH5ghQxEh1eRxvMxto",
   authDomain: "voidlauncher-bab33.firebaseapp.com",
-  projectId: "voidlauncher-bab33"
+  projectId: "voidlauncher-bab33",
+  storageBucket: "voidlauncher-bab33.firebasestorage.app",
+  messagingSenderId: "273997309805",
+  appId: "1:273997309805:web:a43ce719cb30fcf9dc9c64",
+  measurementId: "G-3PSG5C8YYE"
 };
 
 firebase.initializeApp(firebaseConfig);
